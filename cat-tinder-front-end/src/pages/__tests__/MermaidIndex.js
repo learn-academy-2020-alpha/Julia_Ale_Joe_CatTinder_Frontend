b46e6938b30534293c17805acb15eac3f2efd7f7
Julia <jlilianacollins@gmail.com>
2020-03-24 15:45:52 -0700

    added test

--- cat-tinder-front-end/src/pages/__tests__/MermaidIndex.js
+++ cat-tinder-front-end/src/pages/__tests__/MermaidIndex.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom'
 import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import MermaidIndex from '../MermaidIndex'
+import mermaids from '../../mermaids'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 it('MermaidIndex renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<MermaidIndex />, div)
+})
+
+it('Renders the mermaids', ()=>{
+  const component = mount(<CatIndex mermaids={ mermaids }/>)
+  const headings = component.find('h4')
+  expect(headings.length).toBe(3)
 })
