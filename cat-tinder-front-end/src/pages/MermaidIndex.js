b94dcd26a4ac59297ddbb40a96b7845ab83693fb
haizhou85 <60331375+haizhou85@users.noreply.github.com>
2020-03-24 14:25:35 -0700

    Joe setup

---
+++ cat-tinder-front-end/src/pages/MermaidIndex.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class MermaidIndex extends Component{
+  render(){
+    return(
+      <h1>MermaidIndex</h1>
+    )
+  }
+}
+export default MermaidIndex
---
+++ cat-tinder-front-end/src/pages/MermaidShow.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class MermaidShow extends Component{
+  render(){
+    return(
+      <h1>MermaidShow</h1>
+    )
+  }
+}
+export default MermaidShow
